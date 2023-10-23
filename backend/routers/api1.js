6dbf63a61032e483e90ccbcbd6068c8571889618
SantiagoFK <84585956+SantiagoFK@users.noreply.github.com>
2023-10-23 12:16:14 -0300

    Made basic setup of backend folders, installed morgan dependency.

---
+++ backend/routers/api1.js
@@ -0,0 +1,7 @@
+/* version 1 de la api*/
+
+const express = require('express')
+
+const app = express.Router()
+
+module.exports = app
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const apiV1 = require('./routers/api1')
 
 const app = express()
 
-app.use(express.static('public'))
+app.use('/v1', apiV1)
+
+app.get("/", (req, res) => {
+    res.send("homepage")
+})
 
 const server = app.listen(4000, () => {
     console.log("Server listening in port 4000")
